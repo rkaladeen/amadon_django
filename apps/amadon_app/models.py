de55396bd9c0a2591c43abfb5d4acb3136678a6e
raj-kaladeen <r.kaladeen@gmail.com>
2019-07-19 01:45:41 -0400

    finished index page

--- apps/amadon_app/models.py
+++ apps/amadon_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class ProdManager(models.Manager):
+  def prod_available(self, requestedProd, onHand):
+    error = ""
+    if requestedProd > onHand:
+      error = "We are sorry we do not have sufficient amount of product in-stock"
+    return error
+
+class Product(models.Model):
+  item_name = models.CharField(max_length=45)
+  price = models.FloatField()
+  qty_in_stock = models.IntegerField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  objects = ProdManager()
--- apps/amadon_app/views.py
+++ apps/amadon_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def index(request):
+  context = { "all_products" : Product.objects.all() }
+  return render(request, "amadon_app/index.html", context)
+
+def checkout(request):
+  # Function to adjust inventory Here
+
+  # Function to render items bought here
+  
+  return render(request, "amadon_app/checkout.html")
